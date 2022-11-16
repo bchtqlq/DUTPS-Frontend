d7490eeee281c5b4e28093fde83de6b7afacf710
BinhPhanVan <phanvanbinh192837@gmail.com>
2022-11-16 18:55:33 +0700

    Implement for register

--- src/app/pages/auth/register/register.page.ts
+++ src/app/pages/auth/register/register.page.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '@lib/services';
 
 @Component({
   standalone: true,
@@ -7,4 +9,46 @@ import { Component } from '@angular/core';
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.css'],
 })
-export class RegisterPage {}
+export class RegisterPage {
+  private _callbackURL: string;
+  errorInput = '';
+  @ViewChild('username', { static: true }) usernameElement: ElementRef;
+  @ViewChild('password', { static: true }) passwordElement: ElementRef;
+  @ViewChild('confirmPassword', { static: true }) confirmPasswordElement: ElementRef;
+  @ViewChild('email', { static: true }) emailElement: ElementRef;
+  @ViewChild('invalidData') invalidElement: ElementRef;
+  constructor(
+    private _router: Router,
+    private _activatedRoute: ActivatedRoute,
+    private _authService: AuthService,
+    usernameElement: ElementRef,
+    emailElement: ElementRef,
+    passwordElement: ElementRef,
+    confirmPasswordElement: ElementRef,
+    invalidElement: ElementRef
+
+  ) {
+    this._callbackURL = this._activatedRoute.snapshot.queryParamMap.get('callbackURL') || `/`;
+    this.usernameElement = usernameElement;
+    this.passwordElement = passwordElement;
+    this.emailElement = emailElement;
+    this.confirmPasswordElement = confirmPasswordElement;
+    this.invalidElement = invalidElement;
+  }
+  onClickRegister(): void {
+    this.errorInput = '';
+    this.invalidElement.nativeElement.classList.add('invisible');
+    this._authService
+      .register(this.usernameElement.nativeElement.value,this.emailElement.nativeElement.value ,this.passwordElement.nativeElement.value, this.confirmPasswordElement.nativeElement.value)
+      .subscribe(
+        (response) => {
+          this._router.navigate([this._callbackURL]);
+        },
+        (error) => {
+          this.invalidElement.nativeElement.classList.remove('invisible');
+          this.errorInput = 'Invalid data. Please try again!';
+          console.log("error")
+        },
+      );
+  }
+}
